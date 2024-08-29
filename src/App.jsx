1fbbc86e267788a3b6ea97d8ec6a7ae5758ebaf5
kitarou888 <85793702+kitarou888@users.noreply.github.com>
2024-08-29 22:28:20 +0900

    SWRを導入して少しイケてるUI/UXにした

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from "react";
+import useSWR from "swr";
 import "./App.css";
 
 function App() {
   const url = "https://httpstat.us/200?sleep=2000";
   const headers = { Accept: "application/json" };
+  const { data, error, isLoading } = useSWR(url, () =>
+    fetch(url, { headers }).then((res) => res.json())
+  );
 
-  const [status, setStatus] = useState("");
+  if (error) return <p>Failed to load.</p>;
+  if (isLoading) return <p>loading...</p>;
 
-  fetch(url, { headers })
-    .then((res) => res.json())
-    .then((json) => setStatus(json.description));
-
-  return <>{status && <p>Status : {status}</p>}</>;
+  return <p>Status : {data.description}</p>;
 }
 
 export default App;
